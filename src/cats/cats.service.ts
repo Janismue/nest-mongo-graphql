9f63f546df30433d530fcc0e2fe700b1a3544975
Janis Müller <janis_mueller@gmx.de>
2021-01-17 18:50:24 +0100

    little fix

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateCatDto } from './dto/create-cat.dto';
+import { CatType } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
   constructor(@InjectModel('Cat') private readonly catModel: Model<Cat>) {}
 
-  async create(createCatDto: CreateCatDto): Promise<Cat> {
-    const createdCat = new this.catModel(createCatDto);
+  async create(catType: CatType): Promise<Cat> {
+    const createdCat = new this.catModel(catType);
     return await createdCat.save();
   }
 
